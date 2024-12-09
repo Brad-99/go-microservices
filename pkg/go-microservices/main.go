47cc3f7196212c5b8fa0f437585436b4c7566221
Brad <rayray21211@gmail.com>
2024-12-09 19:37:05 +0800

    routing

--- pkg/go-microservices/main.go
+++ pkg/go-microservices/main.go
@@ -2,17 +2,47 @@ package main
 
 import (
     "fmt"
-	"log"
     "net/http"
+
+    "github.com/gorilla/mux"
 )
 
 func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, you've requested: %s with token: %s\n", r.URL.Path, r.URL.Query().Get("token"))
+    r := mux.NewRouter()
+
+    r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+        vars := mux.Vars(r)
+        title := vars["title"]
+        page := vars["page"]
+
+        fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
     })
-	log.Println("Web server has started")
-    http.ListenAndServe(":80", nil)
+
+    http.ListenAndServe(":80", r)
 }
+
+
+// package main
+
+// import (
+//     "fmt"
+// 	"log"
+//     "net/http"
+// )
+
+// func rootHandler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "Hello, you've requested: %s with token: %s\n", r.URL.Path, r.URL.Query().Get("token"))
+// }
+
+// func main() {
+//     http.HandleFunc("/", rootHandler)
+
+// 	fs := http.FileServer(http.Dir("static/"))
+//     http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+// 	log.Println("Web server has started")
+//     http.ListenAndServe(":80", nil)
+// }
 
 
 
